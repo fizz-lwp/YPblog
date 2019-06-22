6fbe6f308bd4926fdf413dc75622d532e8856dd2
Administrator <1848563105@qq.com>
2019-06-22 15:28:26 +0800

    20190622 commit

--- src/main/java/com/blog/service/serviceImpl/BlogServiceImpl.java
+++ src/main/java/com/blog/service/serviceImpl/BlogServiceImpl.java
@@ -31,11 +31,13 @@ public class BlogServiceImpl implements BlogService {
         else{
             Random random = new Random(System.currentTimeMillis());
             List<Blog> blogList = blogMapper.selectRecommendBlogs(typeId); // all or 2*num ?
-            int tnum = Math.min(num,blogList.size());
-            List<Blog> result = new ArrayList<>(tnum);
-            Map<Integer,Integer> booleans = new HashMap<>(tnum);
+            if(blogList.size() < num){
+                num = blogList.size();
+            }
+            List<Blog> result = new ArrayList<>(num);
+            Map<Integer,Integer> booleans = new HashMap<>(num);
             int number;
-            for(int i = 0;i < tnum;i++){
+            for(int i = 0;i < num;i++){
                 do{
                     number = random.nextInt(blogList.size());
                 }while(booleans.containsKey(number));
